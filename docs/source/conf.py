79cb3dc56207c37ee839c9d97159d0a1da522280
Alexander Kmoch <alexander.kmoch@ut.ee>
2025-03-22 20:13:30 +0200

    docs update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ project = 'dggrid4py'
 copyright = '2025, Alexander Kmoch'
 author = "Alexander Kmoch"
 
-release = '0.3.2'
-version = '0.3.2'
+release = '0.3.3'
+version = '0.3.3'
 
 # -- General configuration
 
